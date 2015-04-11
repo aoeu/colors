2b5deb25e6cf07c52cb3beefd96ba474f227f1c6
Travis <travis@mcdem.us>
2015-04-10 20:03:59 -0400

    Add command line flags and superfluous 'features'

--- cmd/gencolors.go
+++ cmd/gencolors.go
@@ -7,13 +7,14 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 var templText = `
 <html>
 	<body>
 	{{range .}}
-	<div style='width: 50px; height: 50px; display: inline-block; background-color: {{safe .}};'>blah</div>
+	<div style='text-align: center; width: 64px; height: 64px; display: inline-block; background-color: {{safe .}};'>{{.}}</div>
 	{{end}}
 	</body>
 </html>
@@ -23,37 +24,51 @@ func preview(c []colors.HSL) {
 	// TODO(aoeu): Implement browser preview here.
 }
 
-
-/*
-
-TODO(aoeu): Something like this is what gets jammed into d3.
-Dump it on a command line? 
-Sprintf out the whole line of js? (No.)
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", 
-            "#a05d56", "#d0743c", "#ff8c00"]);
-
-*/
+func toECMAScriptArray(c []string) string {
+	s := fmt.Sprint("[")
+	for i, cc := range c {
+		if i > 0 {
+			s += fmt.Sprint(",")
+		}
+		s += fmt.Sprintf(`"%v"`, cc)
+	}
+	s += fmt.Sprintln("]")
+	return s
+}
 
 func main() {
-	port := *flag.String("port", ":8081", "The port to serve on")
-	numColors := *flag.Int("num", 20, "The number of colors")
-	saturation := *flag.Float64("saturation", 1.0, "Amount of saturation")
-	lightness := *flag.Float64("lightness", 0.5, "Amount of lightness")
+	// TODO(aoeu): Add flag values as an anonymous struct
+	serve := flag.Bool("preview", false, "Preview this as a web page")
+	port := flag.String("port", ":8081", "The port to serve on")
+	numColors := flag.Int("num", 14, "The number of colors")
+	saturation := flag.Float64("saturation", 0.9, "Amount of saturation")
+	dump := flag.Bool("dump", true, "Dump preview results to standard output.")
+	lightness := flag.Float64("lightness", 0.5, "Amount of lightness")
 	flag.Parse()
-	s := make([]string, numColors)
-	for i, h := range colors.NewHSLSet(numColors, saturation, lightness) {
-		s[i] = fmt.Sprint(h)
+
+	s := make([]string, *numColors)
+	for i, h := range colors.NewHSLSet(*numColors, *saturation, *lightness) {
+		s[i] = fmt.Sprint(h.ToRGB())
 	}
 	funcMap := template.FuncMap{
 		"safe": func(s string) template.CSS {
 			return template.CSS(s)
 		},
 	}
+
+	fmt.Println(toECMAScriptArray(s))
+
+	if !*serve {
+		return
+	}
 	t := template.New("colors").Funcs(funcMap)
 	t, err := t.Parse(templText)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if *dump {
+		t.Execute(os.Stdout, &s)
+		return
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		err := t.Execute(w, &s)
@@ -61,6 +76,5 @@ func main() {
 			log.Fatal(err)
 		}
 	})
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(*port, nil))
 }
-
